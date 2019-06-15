22726c4960bf484b9c20f80ff5608cefccbdbdec
Tyler Hunt <tyhunt04@gmail.com>
2019-06-15 16:52:05 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 // your code here
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
-}
+
 
 function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
